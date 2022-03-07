eae627c6bfbbe93f0f47f792ad1e3b2bff597046
RatMan64 <riley.g.barnes@wsu.edu>
2022-03-07 13:56:26 -0800

    added some debug for server.py

--- server.py
+++ server.py
@@ -15,38 +15,45 @@ print("[+] Server ready to Receive......")
 #Fill in end
 while True:
     #Establish the connection
-    print("[D] im here....")
+    #print("[D] im here....")
     connectionSocket, addr = serverSocket.accept()#Fill in start #Fill in end
-    print("[D] im here....")
+    print("[+]connection established....")
+    #print("[D] im here....")
     try:
-        print("[D] i got in to try...")
+        #print("[D] i got in to try...")
+        print("[+]trying to send hello....")
         message = connectionSocket.recv(1024).decode()#Fill in start #Fill in end
         filename = message.split()[1]
         f = open(str(filename[1:]))
-        outputdata = f.read(1024)#Fill in start #Fill in end
+        outputdata = f.read()#Fill in start #Fill in end
+        print("[+]file read ready to send....")
         #Send one HTTP header line into socket
 
         # print(outputdata)
         #Fill in start
-        header = "HTTP/1.x 200 OK"
+        header = "HTTP/1.x 200 OK\n\n"
         outputdata = header + outputdata
         #Fill in end
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
         connectionSocket.send("\r\n".encode())
+        print("[+]message sent..." )
         connectionSocket.close()
+        print("[*] connection closed...")
     except IOError:
-        print("[D] somthing went wrong...")
+        print("[-]file was not found...")
+        #print("[D] somthing went wrong...")
         #Send response message for file not found
         #Fill in start
-        header = "HTTP/1.x 404 NOT FOUND"
+        header = "HTTP/1.x 404 NOT FOUND\n\n"
         connectionSocket.send(header.encode())
 
         #Fill in end
         #Close client socket
         #Fill in start
         connectionSocket.close()
+        print("[*] connection closed...")
         #Fill in end
 serverSocket.close()
 sys.exit()#Terminate the program after sending the corresponding dat
